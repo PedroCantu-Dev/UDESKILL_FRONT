0a433d5e840163315a85df442b537eaaf2ec9676
PedroCntu-Dev <pedro.cantu_dev@outlook.com>
2023-06-28 15:55:52 -0600

    more folder structure

---
+++ UDESKILL/src/app/features/notebook/components/display/display.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'display-editor',
+  templateUrl: './display-editor.component.html',
+  styleUrls: ['./display-editor.component.scss']
+})
+export class AppComponent {
+
+}
---
+++ UDESKILL/src/app/features/notebook/components/editor/notebook-editor.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'notebook-editor',
+  templateUrl: './notebook-editor.component.html',
+  styleUrls: ['./notebook-editor.component.scss']
+})
+export class AppComponent {
+
+}
